c47e2946abb962232663aa184be0d8b1ce22aa3a
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-01-26 09:21:31 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -19,21 +19,3 @@ async def start_(client: Client, message: Message):
 𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [ᗩɴᴋɪᴛ & Ꮶʜᴜsʜɪ](t.me/UNIQUE_SOCIETY)**""",
     
 
-@Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
-async def help(client: Client, message: Message):
-    await message.reply_photo(
-        caption=f"""""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("❣️ ᗩɴᴋɪᴛ", url=f"t.me/Ankit_shukla_xD"),
-                InlineKeyboardButton("Ꮶʜᴜsʜɪ ❣️", url=f"t.me/khushi_shukla_xD"),
-            ],
-            [
-                InlineKeyboardButton(
-                    "🚑 Տᴜᴘᴘᴏʀᴛ", url=f"https://t.me/UNIQUE_SOCIETY"
-                ),
-                InlineKeyboardButton("Ⴎᴘᴅᴀᴛᴇs 📢", url=f"https://t.me/UNIQUE_LIFELINE"),
-            ],
-        ]
-    )
