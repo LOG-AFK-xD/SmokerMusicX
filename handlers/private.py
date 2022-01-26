a4594edbc44e2d4554bb559e23c1a7ecd1ec8b61
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-01-26 10:37:54 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -10,18 +10,18 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/fc98d41cfda83f4e31cd9.jpg",
-        caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 🎶 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 = [ᴶᵃᶜᵏ×͜×  ˢᵖᵃʳʳᵒʷ](t.me/Its_jack)
+        photo=f"https://telegra.ph/file/55fdbedd3115d8c363d2f.jpg",
+        caption=f"""**⛦➪ Tʜɪs Is Aᴅᴠᴀɴᴄᴇ Tᴇʟᴇɢʀᴀᴍ Mᴜsɪᴄ 🎧 Bᴏᴛ Rᴜɴ Oɴ Pᴜʙʟɪᴄ VPS Sᴇʀᴠᴇʀ 📡 Fᴇᴇʟ Hɪɢʜ Qᴜᴀʟɪᴛʏ ❣️  Mᴜsɪᴄ Iɴ Vᴄ  Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ ➪ [ᗩɴᴋɪᴛ Ꮶʜᴜsʜɪ](t.me/UNIQUE_LIFELINE)
 
-𝐂𝐫𝐞𝐚𝐭𝐨𝐫 :- [ᴶᵃᶜᵏ×͜×  ˢᵖᵃʳʳᵒʷ](t.me/Its_jack)
+⛦➪ Ꮯʀᴇᴀᴛᴏʀ :- [ᗩɴᴋɪᴛ Ꮶʜᴜsʜɪ](t.me/KHUSHI_SHUKLA_XD)
 
 
-𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [ᴶᵃᶜᵏ×͜×  ˢᵖᵃʳʳᵒʷ](t.me/Its_jack)**""",
+⛦➪ Ꭻᴏɪɴ Ꮇʏ Ꮯʜᴀᴛᴛɪɴɢ Ꮐʀᴏᴜᴘ = [❣️ ᗩɴᴋɪᴛ Ꮶʜᴜsʜɪ ❣️](t.me/UNIQUE_SOCIETY)**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "this bot only for this group 👉", url=f"https://t.me/Friendschattinggrp")
+                        "🌸 ᗩᴅᴅ ᗰᴇ Ꭹᴏᴜʀ Ꮐʀᴏᴜᴘ 🌸", url=f"t.me/khushi_robot?startgroup=true")
                 ]
                 
            ]
@@ -37,7 +37,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "💥 Cʟɪᴄᴋ Mᴇ Tᴏ Gᴇᴛ Rᴇᴘᴏ 💞", url=f"https://t.me/Friendschattinggrp")
+                        "💥 Ꭻᴏɪɴ Ꮇʏ Ꮯʜᴀᴛᴛɪɴɢ Ꮐʀᴏᴜᴘ 💞", url=f"https://t.me/UNIQUE_SOCIETY")
                 ]
             ]
         ),
